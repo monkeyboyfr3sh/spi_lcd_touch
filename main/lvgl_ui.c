944f5fb4abea9582d37e1e89db81faf240c2389d
David Cain <david.cain@pelicanengineering.com>
2024-04-07 19:41:56 -0500

    added chart plot

--- main/lvgl_ui.c
+++ main/lvgl_ui.c
@@ -135,6 +135,11 @@ static void set_clock(void)
     lv_meter_set_indicator_end_value(clock_meter, indic_hour, system_minutes);
 }
 
+static lv_obj_t * chart;
+static lv_chart_series_t  * ser1;
+static lv_chart_series_t  * ser2;
+
+
 void create_lvgl_ui(display_mode_t display_mode)
 {
     lv_disp_t *disp = NULL; // This will fetch default
@@ -291,6 +296,44 @@ void create_lvgl_ui(display_mode_t display_mode)
         setup = true;
         break;
     
+    case plot_display:
+        /*Create a chart*/
+        chart = lv_chart_create(lv_scr_act());
+        lv_obj_set_size(chart, 200, 150);
+        lv_obj_center(chart);
+        lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+
+        /*Add two data series*/
+        ser1 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+        ser2 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_GREEN), LV_CHART_AXIS_SECONDARY_Y);
+
+        /*Set the next points on 'ser1'*/
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 10);
+        lv_chart_set_next_value(chart, ser1, 30);
+        lv_chart_set_next_value(chart, ser1, 70);
+        lv_chart_set_next_value(chart, ser1, 90);
+
+        /*Directly set points on 'ser2'*/
+        ser2->y_points[0] = 90;
+        ser2->y_points[1] = 70;
+        ser2->y_points[2] = 65;
+        ser2->y_points[3] = 65;
+        ser2->y_points[4] = 65;
+        ser2->y_points[5] = 65;
+        ser2->y_points[6] = 65;
+        ser2->y_points[7] = 65;
+        ser2->y_points[8] = 65;
+        ser2->y_points[9] = 65;
+
+        lv_chart_refresh(chart); /*Required after direct set*/
+        break;
+
     default:
         display_mode = display_mode_unknown;
         break;
--- main/lvgl_ui.h
+++ main/lvgl_ui.h
@@ -5,6 +5,7 @@ typedef enum {
     bar_display,
     meter_display,
     clock_display,
+    plot_display,
     display_mode_max,
     display_mode_unknown
 } display_mode_t;
