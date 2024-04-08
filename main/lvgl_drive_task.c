4fc01ab1b32c76e78c414e52de3dffd0c38c8249
David Cain <david.cain@pelicanengineering.com>
2024-04-07 20:56:28 -0500

    cleaning up UI

--- main/lvgl_drive_task.c
+++ main/lvgl_drive_task.c
@@ -13,6 +13,7 @@
 #include <driver/i2c.h>
 #include <esp_timer.h>
 #include <lvgl.h>
+#include "esp_task_wdt.h"
 
 #include "display_config.h"
 #include "qmi8658.h"
@@ -51,6 +52,23 @@ static void increase_lvgl_tick_cb(void *arg)
 {
     /* Tell LVGL how many milliseconds has elapsed */
     lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
+}
+
+static void lvgl_update_cb(void *arg)
+{
+    int avg_x = getAccumulatedSum(&buff_x)/buff_x.size;
+    int avg_y = getAccumulatedSum(&buff_y)/buff_y.size;
+    int avg_z = getAccumulatedSum(&buff_z)/buff_z.size;
+    // const float accelerometer_sensitivity = 8192.0;
+    const float accelerometer_sensitivity = 16384.0;
+
+    // Calculate g
+    float x = ((float)avg_x/accelerometer_sensitivity) * 9.81;
+    float y = ((float)avg_y/accelerometer_sensitivity) * 9.81;
+    float z = ((float)avg_z/accelerometer_sensitivity) * 9.81;
+
+    // Update UI
+    set_accelerometer_data(x, y, z);
 }
 
 static void sample_qmi8658_cb(void *arg)
@@ -94,7 +112,7 @@ void lvgl_drive_task(void *arg)
     gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
 
     ESP_LOGI(TAG, "Initializing filters");
-    static size_t buff_len = 75;
+    static size_t buff_len = 100;
     initializeCircularBuffer(&buff_x, buff_len);
     initializeCircularBuffer(&buff_y, buff_len);
     initializeCircularBuffer(&buff_z, buff_len);
@@ -121,20 +139,31 @@ void lvgl_drive_task(void *arg)
     ESP_ERROR_CHECK(esp_timer_create(&lvgl_sample_timer_args, &lvgl_sample_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_sample_timer, LVGL_SAMPLE_PERIOD_US));
 
+    ESP_LOGI(TAG, "Install LVGL update timer");
+    // Tick interface for LVGL (using esp_timer to generate periodic event)
+    const esp_timer_create_args_t lvgl_update_timer_args = {
+        .callback = &lvgl_update_cb,
+        .name = "lvgl_update"
+    };
+
+    esp_timer_handle_t lvgl_update_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_update_timer_args, &lvgl_update_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_update_timer, LVGL_UPDATE_PERIOD_MS * 1000));
+
     ESP_LOGI(TAG, "Creating UI");
     // Lock the mutex due to the LVGL APIs are not thread-safe
     if (lvgl_lock(-1)) {
-        create_lvgl_ui(bar_display);
+        create_lvgl_ui(plot_display);
         // Release the mutex
         lvgl_unlock();
     }
 
     ESP_LOGI(TAG, "Initializing QMI8658");
     qmi8658_reset(I2C_MASTER_NUM);
-    qmi8658_write_byte(I2C_MASTER_NUM, 0x03, 0x13); // Set accelerometer (sensitivity=+-4g) and (data rate=1000Hz)
+    // qmi8658_write_byte(I2C_MASTER_NUM, 0x03, 0x13); // Set accelerometer (sensitivity=+-4g) and (data rate=1000Hz)
+    qmi8658_write_byte(I2C_MASTER_NUM, 0x03, 0x03); // Set accelerometer (sensitivity=+-4g) and (data rate=1000Hz)
     qmi8658_write_byte(I2C_MASTER_NUM, 0x06, 0x01); // Enable accelerometer LPF
     qmi8658_write_byte(I2C_MASTER_NUM, 0x08, 0x01); // Enable accelerometer
-    const float accelerometer_sensitivity = 8192.0;
 
     // Run whoami checl
     if(qmi8658_whoami_check(I2C_MASTER_NUM)!=ESP_OK){
@@ -153,22 +182,9 @@ void lvgl_drive_task(void *arg)
     bool curr_btn_lvl = true;
     while (1) {
         
-        int avg_x = getAccumulatedSum(&buff_x)/buff_x.size;
-        int avg_y = getAccumulatedSum(&buff_y)/buff_y.size;
-        int avg_z = getAccumulatedSum(&buff_z)/buff_z.size;
-    
-        // Calculate g
-        float x = ((float)avg_x/accelerometer_sensitivity) * 9.81;
-        float y = ((float)avg_y/accelerometer_sensitivity) * 9.81;
-        float z = ((float)avg_z/accelerometer_sensitivity) * 9.81;
-        // ESP_LOGI("main","%f, %f, %f", x, y, z);
-
         // Lock the mutex due to the LVGL APIs are not thread-safe
         if (lvgl_lock(-1)) {
-            task_delay_ms = lv_timer_handler();
-
-            // Update UI
-            set_accelerometer_data(x, y, z);
+            lv_timer_handler();
 
             prev_btn_lvl = curr_btn_lvl;
             curr_btn_lvl = gpio_get_level(GPIO_NUM_0);
@@ -184,11 +200,6 @@ void lvgl_drive_task(void *arg)
 
             // Release the mutex
             lvgl_unlock();
-        }
-        if (task_delay_ms > EXAMPLE_LVGL_TASK_MAX_DELAY_MS) {
-            task_delay_ms = EXAMPLE_LVGL_TASK_MAX_DELAY_MS;
-        } else if (task_delay_ms < EXAMPLE_LVGL_TASK_MIN_DELAY_MS) {
-            task_delay_ms = EXAMPLE_LVGL_TASK_MIN_DELAY_MS;
         }
         vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
     }
--- main/lvgl_ui.c
+++ main/lvgl_ui.c
@@ -138,7 +138,21 @@ static void set_clock(void)
 static lv_obj_t * chart;
 static lv_chart_series_t  * ser1;
 static lv_chart_series_t  * ser2;
+static lv_chart_series_t  * ser3;
 
+static void update_plot(void)
+{
+    float new_x;
+    float new_y;
+    float new_z;
+    
+    // Get current acceleromter data
+    get_accelerometer_data(&new_x,&new_y,&new_z);
+
+    lv_chart_set_next_value(chart, ser1, (int)new_x * graph_sensitivity);
+    lv_chart_set_next_value(chart, ser2, (int)new_y * graph_sensitivity);
+    lv_chart_set_next_value(chart, ser3, (int)new_z * graph_sensitivity);
+}
 
 void create_lvgl_ui(display_mode_t display_mode)
 {
@@ -148,7 +162,7 @@ void create_lvgl_ui(display_mode_t display_mode)
     lv_disp_set_rotation(disp, LV_DISP_ROT_270);
 
     const float acceleration = 9.81; // m/s^2
-    const int g_scale = 2; // # of g to show
+    const float g_scale = 1.1; // # of g to show
     const int bar_range = (acceleration * g_scale) * graph_sensitivity; // Applying graph sensitivity scale
     const int bar_height = 45;
     const int bar_offset = 60;
@@ -166,6 +180,10 @@ void create_lvgl_ui(display_mode_t display_mode)
         }
 
         if (current_display_code == meter_display) {
+            lv_timer_del(accelerometer_sample_draw_timer); 
+        }
+
+        if (current_display_code == plot_display) {
             lv_timer_del(accelerometer_sample_draw_timer); 
         }
     }
@@ -297,41 +315,24 @@ void create_lvgl_ui(display_mode_t display_mode)
         break;
     
     case plot_display:
-        /*Create a chart*/
+        /* Create a chart */
         chart = lv_chart_create(lv_scr_act());
         lv_obj_set_size(chart, 200, 150);
         lv_obj_center(chart);
-        lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+        lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /* Show lines and points too */
 
-        /*Add two data series*/
+        // /* Set the range for the primary axis */
+        lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, -bar_range, bar_range);
+        lv_chart_set_point_count(chart, 100);
+
+        /* Add data series for x, y, and z */
         ser1 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
-        ser2 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_GREEN), LV_CHART_AXIS_SECONDARY_Y);
+        ser2 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_GREEN), LV_CHART_AXIS_PRIMARY_Y);
+        ser3 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_BLUE), LV_CHART_AXIS_PRIMARY_Y);
 
-        /*Set the next points on 'ser1'*/
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 10);
-        lv_chart_set_next_value(chart, ser1, 30);
-        lv_chart_set_next_value(chart, ser1, 70);
-        lv_chart_set_next_value(chart, ser1, 90);
-
-        /*Directly set points on 'ser2'*/
-        ser2->y_points[0] = 90;
-        ser2->y_points[1] = 70;
-        ser2->y_points[2] = 65;
-        ser2->y_points[3] = 65;
-        ser2->y_points[4] = 65;
-        ser2->y_points[5] = 65;
-        ser2->y_points[6] = 65;
-        ser2->y_points[7] = 65;
-        ser2->y_points[8] = 65;
-        ser2->y_points[9] = 65;
-
-        lv_chart_refresh(chart); /*Required after direct set*/
+        accelerometer_sample_draw_timer = lv_timer_create(update_plot, 50, NULL);
+        
+        setup = true;
         break;
 
     default:
